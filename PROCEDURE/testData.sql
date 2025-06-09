9fa826ec914f6816e45b7efebe365cf03ff99cce
Yoon Sejin <pentum99@gmail.com>
2025-06-09 10:51:49 +0900

    file upload

---
+++ PROCEDURE/testData.sql
@@ -0,0 +1,142 @@
+<< 유저 목록 >>
+1e0cb6f9-43c2-11f0-a5ac-00155dfa4261	admin	2025-06-08 02:09:05
+1e0cc793-43c2-11f0-a5ac-00155dfa4261	user	2025-06-08 02:09:05
+1e0cc848-43c2-11f0-a5ac-00155dfa4261	user	2025-06-08 02:09:05
+1e0cc8a2-43c2-11f0-a5ac-00155dfa4261	user	2025-06-08 02:09:05
+1e0cc911-43c2-11f0-a5ac-00155dfa4261	user	2025-06-08 02:09:05
+1e0cc95d-43c2-11f0-a5ac-00155dfa4261	company	2025-06-08 02:09:05
+1e0cc9fe-43c2-11f0-a5ac-00155dfa4261	company	2025-06-08 02:09:05
+1e0cca3f-43c2-11f0-a5ac-00155dfa4261	company	2025-06-08 02:09:05
+
+<< 회사 타입 목록 >>
+1	Startup	초기 단계의 혁신적 기업
+2	Enterprise	대기업 및 중견기업
+3	Non-Profit	비영리 단체
+4	Government	공공 기관
+5	Agency	컨설팅 또는 에이전시
+
+<< 회사 목록 >>
+1e0cc95d-43c2-11f0-a5ac-00155dfa4261	1	InnovateX	https://www.innovatex.com	STP	2015	02-1234-5678	contact@innovatex.com
+1e0cc9fe-43c2-11f0-a5ac-00155dfa4261	2	BlueChip Solutions	https://www.bluechip.co.kr	ENT	2000	02-2345-6789	info@bluechip.co.kr
+1e0cca3f-43c2-11f0-a5ac-00155dfa4261	5	Prime Agency	https://www.primeagency.kr	AGY	1990	02-3456-7890	hello@primeagency.kr
+
+<< 회사 지점 목록 >>
+d3cfe649-43c2-11f0-a5ac-00155dfa4261	1e0cc95d-43c2-11f0-a5ac-00155dfa4261	InnovateX HQ	123 Teheran-ro, Gangnam-gu, Seoul, Korea	Seoul	02-1111-2222	1	1	2025-06-08 02:14:10	2025-06-08 02:14:10
+d3cfe9c5-43c2-11f0-a5ac-00155dfa4261	1e0cc95d-43c2-11f0-a5ac-00155dfa4261	InnovateX Busan Office	45 Haeundaehaebyeon-ro, Haeundae-gu, Busan, Korea	Busan	051-123-4567	0	1	2025-06-08 02:14:10	2025-06-08 02:14:10
+d3cfeadf-43c2-11f0-a5ac-00155dfa4261	1e0cc9fe-43c2-11f0-a5ac-00155dfa4261	BlueChip HQ	200 Jungang-daero, Jung-gu, Busan, Korea	Busan	051-234-5678	1	1	2025-06-08 02:14:10	2025-06-08 02:14:10
+d3cfeb7a-43c2-11f0-a5ac-00155dfa4261	1e0cc9fe-43c2-11f0-a5ac-00155dfa4261	BlueChip Daegu Office	10 Dongseong-ro, Jung-gu, Daegu, Korea	Daegu	053-345-6789	0	1	2025-06-08 02:14:10	2025-06-08 02:14:10
+d3cfec05-43c2-11f0-a5ac-00155dfa4261	1e0cca3f-43c2-11f0-a5ac-00155dfa4261	Prime Agency HQ	77 Bupyeong-daero, Bupyeong-gu, Incheon, Korea	Incheon	032-456-7890	1	1	2025-06-08 02:14:10	2025-06-08 02:14:10
+d3cfec8a-43c2-11f0-a5ac-00155dfa4261	1e0cca3f-43c2-11f0-a5ac-00155dfa4261	Prime Agency Gwangju Office	5 Nongseon-ro, Buk-gu, Gwangju, Korea	Gwangju	062-567-8901	0	1	2025-06-08 02:14:10	2025-06-08 02:14:10
+
+<< 회사 지점 부서 목록 >> 
+01a3706f-43c3-11f0-a5ac-00155dfa4261	d3cfe649-43c2-11f0-a5ac-00155dfa4261	Engineering	제품 개발 및 시스템 운영을 담당하는 부서
+01a373fd-43c3-11f0-a5ac-00155dfa4261	d3cfe649-43c2-11f0-a5ac-00155dfa4261	Human Resources	인사 관리 및 채용 업무를 담당하는 부서
+01a374c8-43c3-11f0-a5ac-00155dfa4261	d3cfe9c5-43c2-11f0-a5ac-00155dfa4261	Sales	부산 지역 영업 및 고객 관리를 담당하는 부서
+01a3754f-43c3-11f0-a5ac-00155dfa4261	d3cfe9c5-43c2-11f0-a5ac-00155dfa4261	Support	고객 지원 및 기술 지원을 담당하는 부서
+01a375bc-43c3-11f0-a5ac-00155dfa4261	d3cfeadf-43c2-11f0-a5ac-00155dfa4261	Marketing	시장 조사 및 마케팅 전략을 담당하는 부서
+01a37627-43c3-11f0-a5ac-00155dfa4261	d3cfeadf-43c2-11f0-a5ac-00155dfa4261	Finance	회계 및 재무 관리를 담당하는 부서
+01a37691-43c3-11f0-a5ac-00155dfa4261	d3cfeb7a-43c2-11f0-a5ac-00155dfa4261	R&D	연구 개발 및 기술 혁신을 담당하는 부서
+01a376f4-43c3-11f0-a5ac-00155dfa4261	d3cfeb7a-43c2-11f0-a5ac-00155dfa4261	Admin	행정 업무 및 지원을 담당하는 부서
+01a377a1-43c3-11f0-a5ac-00155dfa4261	d3cfec05-43c2-11f0-a5ac-00155dfa4261	Creative	광고 및 크리에이티브 콘텐츠 제작을 담당하는 부서
+01a3780e-43c3-11f0-a5ac-00155dfa4261	d3cfec05-43c2-11f0-a5ac-00155dfa4261	Client Services	클라이언트 커뮤니케이션을 담당하는 부서
+01a37874-43c3-11f0-a5ac-00155dfa4261	d3cfec8a-43c2-11f0-a5ac-00155dfa4261	Operations	프로젝트 운영 및 일정 관리를 담당하는 부서
+01a378db-43c3-11f0-a5ac-00155dfa4261	d3cfec8a-43c2-11f0-a5ac-00155dfa4261	QA	품질 보증 및 검수 업무를 담당하는 부서
+
+<< 유저 팀 목록 >>
+4ab019ae-43c3-11f0-a5ac-00155dfa4261	Alpha Developers
+4ab01c17-43c3-11f0-a5ac-00155dfa4261	Beta Testers
+4ab01cba-43c3-11f0-a5ac-00155dfa4261	Gamma Innovators
+
+<< 자격증 목록 >>
+1	AWS Certified Solutions Architect – Associate	Amazon
+2	Certified Information Systems Security Professional (CISSP)	ISC²
+4	Google Professional Cloud Architect	Google
+5	Microsoft Certified: Azure Fundamentals	Microsoft
+3	Oracle Certified Professional, Java SE 11 Developer	Oracle
+
+<< 스킬 카테고리 목록 >>
+1	Frontend Development
+2	Backend Development
+3	DevOps
+4	Data Science
+5	Security
+
+<< 스킬 목록 >>
+1	1	HTML
+2	1	CSS
+3	1	JavaScript
+4	2	Java
+5	2	Python
+6	2	Node.js
+7	3	Docker
+8	3	Kubernetes
+9	4	SQL
+10	4	R
+11	4	Python
+12	5	Penetration Testing
+13	5	OWASP Top 10
+
+<< 직종 목록 >>
+1	Software Engineer
+2	Data Scientist
+3	DevOps Engineer
+4	Product Manager
+5	UX/UI Designer
+
+<< 공고 목록 >>
+1	d3cfe649-43c2-11f0-a5ac-00155dfa4261	1	1e0cc95d-43c2-11f0-a5ac-00155dfa4261	Frontend Software Engineer	HTML/CSS/JavaScript 기반의 웹 프론트엔드 개발 및 유지보수 담당자를 찾습니다.	2025-06-09	2025-07-09	hr@innovatex.com	OPEN	60000000	2
+2	d3cfe9c5-43c2-11f0-a5ac-00155dfa4261	3	1e0cc95d-43c2-11f0-a5ac-00155dfa4261	DevOps Engineer	Docker, Kubernetes를 이용한 CI/CD 파이프라인 구축 및 운영 담당자 모집.	2025-06-10	2025-08-10	hr@innovatex.com	OPEN	65000000	3
+3	d3cfeadf-43c2-11f0-a5ac-00155dfa4261	2	1e0cc9fe-43c2-11f0-a5ac-00155dfa4261	Data Scientist	SQL/R/Python 활용한 데이터 분석 및 모델 개발 경험자 우대.	2025-06-11	2025-07-11	recruit@bluechip.co.kr	OPEN	70000000	2
+4	d3cfec05-43c2-11f0-a5ac-00155dfa4261	5	1e0cca3f-43c2-11f0-a5ac-00155dfa4261	UX/UI Designer	Figma/Sketch 활용한 사용자 경험 및 인터페이스 디자인 담당자 모집.	2025-06-12	2025-07-12	contact@primeagency.kr	OPEN	55000000	1
+
+<< 옵션 카테고리 목록 >>
+1	Work Location	선호하는 근무 장소(예: 원격, 사무실, 하이브리드)
+2	Employment Type	고용 형태(예: 정규직, 계약직, 인턴)
+3	Team Size	희망 팀 규모(예: 소규모, 중규모, 대규모)
+4	Industry	관심 산업 분야(예: 금융, 게임, 헬스케어)
+5	Seniority Level	경력 수준(예: 주니어, 미드, 시니어)
+
+<< 옵션 목록 >>
+1	1	Remote	완전 원격 근무
+2	1	On-site	출근 중심 사무실 근무
+3	1	Hybrid	하이브리드(원격 + 사무실)
+4	2	Full-time	정규직
+5	2	Contract	계약직
+6	2	Internship	인턴십
+7	3	Small (1-10)	소규모 팀
+8	3	Medium (11-50)	중규모 팀
+9	3	Large (51+)	대규모 팀
+10	4	Finance	금융
+11	4	Gaming	게임
+12	4	Healthcare	헬스케어
+13	5	Junior	주니어
+14	5	Mid-level	미드 레벨
+15	5	Senior	시니어
+
+<< 유저 선호 id 목록 >>
+ef71bc9b-43c5-11f0-a5ac-00155dfa4261	1e0cc793-43c2-11f0-a5ac-00155dfa4261	1	40000000	60000000
+ef71c188-43c5-11f0-a5ac-00155dfa4261	1e0cc793-43c2-11f0-a5ac-00155dfa4261	2	50000000	70000000
+ef71c25b-43c5-11f0-a5ac-00155dfa4261	1e0cc848-43c2-11f0-a5ac-00155dfa4261	5	30000000	50000000
+ef71c2fa-43c5-11f0-a5ac-00155dfa4261	1e0cc8a2-43c2-11f0-a5ac-00155dfa4261	4	45000000	65000000
+ef71c372-43c5-11f0-a5ac-00155dfa4261	1e0cc911-43c2-11f0-a5ac-00155dfa4261	3	35000000	55000000
+
+<< 성향 목록 >>
+1	Team Player	팀워크와 협업을 중요시하는 성향
+2	Problem Solver	문제 해결 능력이 뛰어난 성향
+3	Detail Oriented	세부사항에 주의를 기울이는 성향
+4	Innovative	창의적이고 혁신적인 아이디어를 추구하는 성향
+5	Adaptable	변화에 유연하게 대응하는 성향
+
+<< 챌린지 카테고리 목록 >>
+1	Hackathon	사내·외 해커톤 및 아이디어 경진대회 카테고리
+2	Bug Bounty	보안 취약점 발견 및 보고 챌린지 카테고리
+3	Feature Sprint	새로운 기능 설계·개발 집중 챌린지 카테고리
+4	Design Sprint	UI/UX 및 서비스 개선을 위한 디자인 집중 챌린지
+5	Performance Test	시스템 성능 측정 및 최적화 챌린지
+
+<< 챌린지 목록 >>
+1	1e0cc95d-43c2-11f0-a5ac-00155dfa4261	1	1e0cc95d-43c2-11f0-a5ac-00155dfa4261	InnovateX Global Hackathon 2025	1
+2	1e0cc95d-43c2-11f0-a5ac-00155dfa4261	2	1e0cc95d-43c2-11f0-a5ac-00155dfa4261	InnovateX Spring Bug Bounty	0
+3	1e0cc9fe-43c2-11f0-a5ac-00155dfa4261	3	1e0cc9fe-43c2-11f0-a5ac-00155dfa4261	BlueChip Q3 Feature Sprint	1
+4	1e0cc9fe-43c2-11f0-a5ac-00155dfa4261	4	1e0cc9fe-43c2-11f0-a5ac-00155dfa4261	BlueChip UX Design Sprint	0
+5	1e0cca3f-43c2-11f0-a5ac-00155dfa4261	5	1e0cca3f-43c2-11f0-a5ac-00155dfa4261	Prime Agency Performance Blitz	0
